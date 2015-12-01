2ced5b9cea14239340eb08b81801f9eab5288619
Timur Catakli <timurcatakli@gmail.com>
2015-12-01 16:27:32 +0200

    Update reflections and challenges

---
+++ week-4/mini-challenges.rb
@@ -0,0 +1,10 @@
+#Hours in a year
+hours_in_one_day = 24
+hours_in_a_year = hours_in_one_day * 365
+puts "There are #{hours_in_a_year} hours in a year - ignoring leap years"
+
+#Minutes in a decade
+
+decade_days = 10 * 365			#ignoring leap years
+minutes_in_a_day = 1 * 60 * 24
+puts "There are #{decade_days * minutes_in_a_day} minutes in a decade"
