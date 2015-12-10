deb8c322e0612e9503307c48471bff23e50ecbf4
Timur Catakli <timurcatakli@gmail.com>
2015-12-10 11:01:05 +0200

    Solve commas challenge

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -1,6 +1,6 @@
 # Research Methods
 
-# I spent [] hours on this challenge.
+# I spent [1.5] hours on this challenge.
 
 i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
 my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
@@ -15,9 +15,13 @@ def my_hash_finding_method(source, thing_to_find)
 end
 
 # Identify and describe the Ruby method(s) you implemented.
+# .map!: Invokes the given block once for each element of self, replacing the element with the value returned by the block.
+# .each: Calls the given block once for each element in self, passing that element as a parameter.
 #
-#
-#
+# Teach your accountability group how to use the methods
+# Please visit http://ruby-doc.org/ in order to find related examples.
+# Share any tricks you used to find and decipher the Ruby Docs
+# Stackoverflow is also a good resource.
 
 # Person 2
 def my_array_modification_method!(source, thing_to_modify)
@@ -39,7 +43,7 @@ def my_hash_modification_method!(source, thing_to_modify)
   end
 end
 
-print my_hash_modification_method!(my_family_pets_ages, 2)
+my_hash_modification_method!(my_family_pets_ages, 2)
 
 # Identify and describe the Ruby method(s) you implemented.
 #
--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -1,6 +1,6 @@
 # Numbers to Commas Solo Challenge
 
-# I spent [] hours on this challenge.
+# I spent [6] hours on this challenge.
 
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
@@ -35,34 +35,34 @@
 
 
 # 1. Initial Solution
-def separate_comma(int)
+# def separate_comma(int)
 	
-	str = int.to_s
-	length = str.length	
-	result = ''
+# 	str = int.to_s
+# 	length = str.length	
+# 	result = ''
 
-	cons = 0
-	arr_str = []
+# 	cons = 0
+# 	arr_str = []
 
-	str.chars.reverse_each do |chr|
-		if cons % 3 == 0 && cons != 0
-			chr = chr + ","
-			arr_str << chr
-		else
-			arr_str << chr
-		end
-		cons += 1
-	end
+# 	str.chars.reverse_each do |chr|
+# 		if cons % 3 == 0 && cons != 0
+# 			chr = chr + ","
+# 			arr_str << chr
+# 		else
+# 			arr_str << chr
+# 		end
+# 		cons += 1
+# 	end
 	
-	arr_str.reverse.each do |rhc|
-	 	result += rhc
-	end
+# 	arr_str.reverse.each do |rhc|
+# 	 	result += rhc
+# 	end
 
-	return result
-end
+# 	return result
+# end
 
 
-puts separate_comma(4049)
+# puts separate_comma(4049)
 # puts separate_comma(13155)
 # puts separate_comma(538542)
 # puts separate_comma(1234)
@@ -77,8 +77,56 @@ puts separate_comma(4049)
 
 
 # 2. Refactored Solution
+def separate_comma(int)
 
+	formatted_int = int.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse
+	return formatted_int
+
+end
+
+
+puts separate_comma(4049)
+puts separate_comma(13155)
+puts separate_comma(538542)
+puts separate_comma(1234)
+puts separate_comma(37167004)
+puts separate_comma(561966179)
+puts separate_comma(5874175451)
+puts separate_comma(18528491078)
+puts separate_comma(738236319073)
+puts separate_comma(2028990772450)
+puts separate_comma(83691472341522)
+puts separate_comma(190508477190784)
+
+# 4049
+# 13155
+# 538542
+# 1234
+# 37167004
+# 561966179
+# 5874175451
+# 18528491078
+# 738236319073
+# 2028990772450
+# 83691472341522
+# 190508477190784
 
 
 
 # 3. Reflection
+# What was your process for breaking the problem down? What different approaches did you consider?
+# I opened up an excel file and tried to find a pattern to come up with an algorithm
+
+# Was your pseudocode effective in helping you build a successful initial solution?
+# Yes
+
+
+# What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
+# I am a big believer of regex and I knew that there has to be a solution. SO that is what I did...
+
+
+# How did you initially iterate through the data structure?
+# by using each
+
+# Do you feel your refactored solution is more readable than your initial solution? Why?
+# 24 lines of code versus 2 lines of code. You decide.
