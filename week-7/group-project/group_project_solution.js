0e1c1d787ce91523f7613200412238f5aa3b3975
Timur Catakli <timurcatakli@gmail.com>
2015-12-29 10:36:38 +0200

    Add group project user story file

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,12 @@
+
+// As a/an		I want to								so that...
+// -------		-----------------------------------		-----------------------------------------------------
+// user		write an empty function named sum		I can later use it
+// user		use the sum function					to sum up all odd length array elements
+// user		use the sum function					to sum up all even length array elements
+// user		write an empty function named mean		I can later use it
+// user		use the mean function					mean return the average of odd length array elements
+// user		use the mean function					mean return the average of even length array elements
+// user		write an empty function named median	I can later use it
+// user		use the median function					median return the median value of odd length array elements
+// user		use the median function					median return the median value of even length array elements
